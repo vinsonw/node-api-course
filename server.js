97404b5941b8e78d4abbb3fde8512bd397c7c409
vinsonw <me@vinsonwei.com>
2022-10-01 04:53:58 +0800

    set a status code

--- server.js
+++ server.js
@@ -7,29 +7,20 @@ const todos = [
 ]
 
 const server = http.createServer((req, res) => {
-  // 设置响应头，客户端才能正确解析返回的内容
-  // res.setHeader('Content-Type', 'text/plain')
-  res.setHeader('Content-Type', 'application/json')
-  // 设置服务器类型
-  res.setHeader('X-Powered-By', 'Express')
-  // 自定义的响应头字段也会出现在浏览器的response headers中
-  res.setHeader('madeup', 'whatever')
 
-
-  // res.end('<h1>hello world!</h1>')
-
-  // 直接响应对象会报错
-  // TypeError [ERR_INVALID_ARG_TYPE]: The "chunk" argument must be of type string or an instance of Buffer or Uint8Array.
-  // res.end({
-  //   success: true,
-  //   data: todos
-  // })
+  // setHeaders的合并写法
+  // 第一个参数是status code
+  res.writeHead(400, {
+    'Content-Type': 'application/json',
+    'X-Powered-By': 'Node.js'
+  })
 
 
   // 将对象转化为JSON字符串再发送
   res.end(JSON.stringify({
-    success: true,
-    data: todos
+    success: false,
+    data: null,
+    msg: 'Please add email'
   }))
 })
 
