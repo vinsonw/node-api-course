4f6280bf844d130427afa508ee7101f148414c5e
vinsonw <me@vinsonwei.com>
2022-10-01 12:16:19 +0800

    the raw body is a bufffer -> string -> object

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const { brotliDecompressSync } = require('zlib');
 
 const todos = [
   {id: 1, text: 'Todo one'},
@@ -9,17 +8,8 @@ const todos = [
 
 const server = http.createServer((req, res) => {
 
-  // setHeaders的合并写法
-  // 第一个参数是status code
-  res.writeHead(400, {
-    'Content-Type': 'application/json',
-    'X-Powered-By': 'Node.js'
-  })
-
-  // 打印请求头中的Authorization字段
-  console.log('--Authorization', req.headers.authorization);
-
-  // 接收Post请求体中的内容
+  const {headers, url, method} = req
+  // 接收请求体中的内容
   let body = []
   req
     .on('data', chunk => { // 每次接收数据时触发
@@ -27,15 +17,42 @@ const server = http.createServer((req, res) => {
     })
     .on('end', () => { // 数据发送完毕以后触发
       body = Buffer.concat(body).toString()
-      console.log(body);
+
+      // 准备响应的默认状态
+      let status = 404
+      const response = {
+        success: false,
+        data: null
+      }
+
+      // 根据请求修改响应的默认状态
+      if (method==='GET' && url==='/todos') {
+        status = 200
+        response.success = true
+        response.data = todos
+      } else if (method==='POST' && url==='/todos') {
+        // 注意body此时是字符串
+        const {id, text} = JSON.parse(body)
+        if (!id || !text) {
+          status = 404
+        } else {
+          status = 200
+          todos.push({ id, text })
+          response.success = true
+          response.data = todos
+        }
+      }
+
+      // 响应修改后的数据
+      res.writeHead(status, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js'
+      })
+
+      // 将对象转化为JSON字符串再发送
+      res.end(JSON.stringify(response))
     })
 
-  // 将对象转化为JSON字符串再发送
-  res.end(JSON.stringify({
-    success: false,
-    data: null,
-    msg: 'Please add email'
-  }))
 })
 
 const PORT = 4000
